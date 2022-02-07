677490b006f60c8c606ddaad5b351907e279da3e
ankur-devfactory <97214200+ankur-devfactory@users.noreply.github.com>
2022-02-07 13:50:47 +0530

    Update config.js

--- Task-1 Hands-on Exercise/js/config.js
+++ Task-1 Hands-on Exercise/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ftEOIGhk8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7nge4m67gu7r9tusghud3m9o2a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://tpm-2009-user-pool.auth.us-east-1.amazoncognito.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
